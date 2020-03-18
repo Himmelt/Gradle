9d8a2540272d68033104446a562b8250703debb5
Himmelt <master@void-3.cn>
2020-03-18 15:40:20 +0800

    update 4.3.1

--- src/main/java/org/gradle/internal/resource/transport/http/HttpClientHelper.java
+++ src/main/java/org/gradle/internal/resource/transport/http/HttpClientHelper.java
@@ -16,48 +16,39 @@
 
 package org.gradle.internal.resource.transport.http;
 
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.Iterables;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.apache.commons.lang.StringUtils;
-import org.apache.http.HttpHeaders;
+import org.apache.http.*;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpHead;
 import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.client.utils.HttpClientUtils;
+import org.apache.http.entity.BufferedHttpEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
-import org.gradle.internal.UncheckedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.CheckForNull;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.Files.readAllBytes;
-import static org.apache.http.client.protocol.HttpClientContext.REDIRECT_LOCATIONS;
 
 /**
  * Provides some convenience and unified logging.
- *
- * @author Himmelt
  */
 public class HttpClientHelper implements Closeable {
 
@@ -112,46 +103,59 @@ public class HttpClientHelper implements Closeable {
         }
     }
 
-    private HttpClientResponse performRawHead(String source, boolean revalidate) {
+    public CloseableHttpResponse performRawHead(String source, boolean revalidate) {
         return performRequest(new HttpHead(source), revalidate);
     }
 
-    public HttpClientResponse performHead(String source, boolean revalidate) {
-        return processResponse(performRawHead(source, revalidate));
+    public CloseableHttpResponse performHead(String source, boolean revalidate) {
+        return processResponse(source, "HEAD", performRawHead(source, revalidate));
     }
 
-    HttpClientResponse performRawGet(String source, boolean revalidate) {
+    public CloseableHttpResponse performRawGet(String source, boolean revalidate) {
         return performRequest(new HttpGet(source), revalidate);
     }
 
-    public HttpClientResponse performGet(String source, boolean revalidate) {
-        return processResponse(performRawGet(source, revalidate));
+    public CloseableHttpResponse performGet(String source, boolean revalidate) {
+        return processResponse(source, "GET", performRawGet(source, revalidate));
     }
 
-    public HttpClientResponse performRequest(HttpRequestBase request, boolean revalidate) {
+    public CloseableHttpResponse performRequest(HttpRequestBase request, boolean revalidate) {
         String method = request.getMethod();
         if (revalidate) {
             request.addHeader(HttpHeaders.CACHE_CONTROL, "max-age=0");
         }
+        CloseableHttpResponse response;
         try {
-            return executeGetOrHead(request);
-        } catch (FailureFromRedirectLocation e) {
-            throw new HttpRequestException(String.format("Could not %s '%s'.", method, stripUserCredentials(e.getLastRedirectLocation())), e.getCause());
+            response = executeGetOrHead(request);
         } catch (IOException e) {
-            throw new HttpRequestException(String.format("Could not %s '%s'.", method, stripUserCredentials(request.getURI())), e);
+            throw new HttpRequestException(String.format("Could not %s '%s'.", method, request.getURI()), e);
         }
-    }
 
-    protected HttpClientResponse executeGetOrHead(HttpRequestBase method) throws IOException {
-        HttpClientResponse response = performHttpRequest(method);
-        // Consume content for non-successful, responses. This avoids the connection being left open.
-        if (!response.wasSuccessful()) {
-            response.close();
-        }
         return response;
     }
 
-    public HttpClientResponse performHttpRequest(HttpRequestBase request) throws IOException {
+    protected CloseableHttpResponse executeGetOrHead(HttpRequestBase method) throws IOException {
+        final CloseableHttpResponse httpResponse = performHttpRequest(method);
+        // Consume content for non-successful, responses. This avoids the connection being left open.
+        if (!wasSuccessful(httpResponse)) {
+            CloseableHttpResponse response = new AutoClosedHttpResponse(httpResponse);
+            HttpClientUtils.closeQuietly(httpResponse);
+            return response;
+        }
+        return httpResponse;
+    }
+
+    public boolean wasMissing(CloseableHttpResponse response) {
+        int statusCode = response.getStatusLine().getStatusCode();
+        return statusCode == 404;
+    }
+
+    public boolean wasSuccessful(CloseableHttpResponse response) {
+        int statusCode = response.getStatusLine().getStatusCode();
+        return statusCode >= 200 && statusCode < 400;
+    }
+
+    public CloseableHttpResponse performHttpRequest(HttpRequestBase request) throws IOException {
         if (sharedContext == null) {
             // There's no authentication involved, requests can be done concurrently
             return performHttpRequest(request, new BasicHttpContext());
@@ -172,10 +176,10 @@ public class HttpClientHelper implements Closeable {
         return context;
     }
 
-    private HttpClientResponse performHttpRequest(HttpRequestBase request, HttpContext httpContext) throws IOException {
+    private CloseableHttpResponse performHttpRequest(HttpRequestBase request, HttpContext httpContext) throws IOException {
         // Without this, HTTP Client prohibits multiple redirects to the same location within the same context
-        httpContext.removeAttribute(REDIRECT_LOCATIONS);
-        LOGGER.debug("Performing HTTP {}: {}", request.getMethod(), stripUserCredentials(request.getURI()));
+        httpContext.removeAttribute(HttpClientContext.REDIRECT_LOCATIONS);
+        LOGGER.debug("Performing HTTP {}: {}", request.getMethod(), request.getURI());
 
         ///////////////////////////////////////////////////////////////////////////
         try {
@@ -189,53 +193,17 @@ public class HttpClientHelper implements Closeable {
         }
         //////////////////////////////////////////////////////////////////////////
 
-        try {
-            CloseableHttpResponse response = getClient().execute(request, httpContext);
-            return toHttpClientResponse(request, httpContext, response);
-        } catch (IOException e) {
-            validateRedirectChain(httpContext);
-            URI lastRedirectLocation = stripUserCredentials(getLastRedirectLocation(httpContext));
-            throw (lastRedirectLocation == null) ? e : new FailureFromRedirectLocation(lastRedirectLocation, e);
-        }
+        return getClient().execute(request, httpContext);
     }
 
-    private HttpClientResponse toHttpClientResponse(HttpRequestBase request, HttpContext httpContext, CloseableHttpResponse response) {
-        validateRedirectChain(httpContext);
-        URI lastRedirectLocation = getLastRedirectLocation(httpContext);
-        URI effectiveUri = lastRedirectLocation == null ? request.getURI() : lastRedirectLocation;
-        return new HttpClientResponse(request.getMethod(), effectiveUri, response);
-    }
-
-    /**
-     * Validates that no redirect used an insecure protocol.
-     * Redirecting through an insecure protocol can allow for a MITM redirect to an attacker controlled HTTPS server.
-     */
-    private void validateRedirectChain(HttpContext httpContext) {
-        settings.getRedirectVerifier().validateRedirects(getRedirectLocations(httpContext));
-    }
-
-    @Nonnull
-    private static List<URI> getRedirectLocations(HttpContext httpContext) {
-        @SuppressWarnings("unchecked")
-        List<URI> redirects = (List<URI>) httpContext.getAttribute(REDIRECT_LOCATIONS);
-        return redirects == null ? Collections.emptyList() : redirects;
-    }
-
-
-    private static URI getLastRedirectLocation(HttpContext httpContext) {
-        List<URI> redirectLocations = getRedirectLocations(httpContext);
-        return redirectLocations.isEmpty() ? null : Iterables.getLast(redirectLocations);
-    }
-
-    private HttpClientResponse processResponse(HttpClientResponse response) {
-        if (response.wasMissing()) {
-            LOGGER.info("Resource missing. [HTTP {}: {}]", response.getMethod(), stripUserCredentials(response.getEffectiveUri()));
+    private CloseableHttpResponse processResponse(String source, String method, CloseableHttpResponse response) {
+        if (wasMissing(response)) {
+            LOGGER.info("Resource missing. [HTTP {}: {}]", method, source);
             return null;
         }
-        if (!response.wasSuccessful()) {
-            URI effectiveUri = stripUserCredentials(response.getEffectiveUri());
-            LOGGER.info("Failed to get resource: {}. [HTTP {}: {})]", response.getMethod(), response.getStatusLine(), effectiveUri);
-            throw new HttpErrorStatusCodeException(response.getMethod(), effectiveUri.toString(), response.getStatusLine().getStatusCode(), response.getStatusLine().getReasonPhrase());
+        if (!wasSuccessful(response)) {
+            LOGGER.info("Failed to get resource: {}. [HTTP {}: {}]", method, response.getStatusLine(), source);
+            throw new HttpErrorStatusCodeException(method, source, response.getStatusLine().getStatusCode(), response.getStatusLine().getReasonPhrase());
         }
 
         return response;
@@ -244,6 +212,7 @@ public class HttpClientHelper implements Closeable {
     private synchronized CloseableHttpClient getClient() {
         if (client == null) {
             HttpClientBuilder builder = HttpClientBuilder.create();
+            builder.setRedirectStrategy(new AlwaysRedirectRedirectStrategy());
             new HttpClientConfigurer(settings).configure(builder);
             this.client = builder.build();
         }
@@ -260,33 +229,155 @@ public class HttpClientHelper implements Closeable {
         }
     }
 
-    /**
-     * Strips the {@link URI#getUserInfo() user info} from the {@link URI} making it
-     * safe to appear in log messages.
-     */
-    @Nullable
-    @VisibleForTesting
-    static URI stripUserCredentials(@CheckForNull URI uri) {
-        if (uri == null) {
-            return null;
-        }
-        try {
-            return new URIBuilder(uri).setUserInfo(null).build();
-        } catch (URISyntaxException e) {
-            throw UncheckedException.throwAsUncheckedException(e, true);
-        }
-    }
+    private static class AutoClosedHttpResponse implements CloseableHttpResponse {
+        private final HttpEntity entity;
+        private final CloseableHttpResponse httpResponse;
 
-    private static class FailureFromRedirectLocation extends IOException {
-        private final URI lastRedirectLocation;
-
-        private FailureFromRedirectLocation(URI lastRedirectLocation, Throwable cause) {
-            super(cause);
-            this.lastRedirectLocation = lastRedirectLocation;
+        public AutoClosedHttpResponse(CloseableHttpResponse httpResponse) throws IOException {
+            this.httpResponse = httpResponse;
+            HttpEntity entity = httpResponse.getEntity();
+            this.entity = entity != null ? new BufferedHttpEntity(entity) : null;
         }
 
-        private URI getLastRedirectLocation() {
-            return lastRedirectLocation;
+        @Override
+        public void close() throws IOException {
+        }
+
+        @Override
+        public StatusLine getStatusLine() {
+            return httpResponse.getStatusLine();
+        }
+
+        @Override
+        public void setStatusLine(StatusLine statusline) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void setStatusLine(ProtocolVersion ver, int code) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void setStatusLine(ProtocolVersion ver, int code, String reason) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void setStatusCode(int code) throws IllegalStateException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void setReasonPhrase(String reason) throws IllegalStateException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public HttpEntity getEntity() {
+            return entity;
+        }
+
+        @Override
+        public void setEntity(HttpEntity entity) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Locale getLocale() {
+            return httpResponse.getLocale();
+        }
+
+        @Override
+        public void setLocale(Locale loc) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public ProtocolVersion getProtocolVersion() {
+            return httpResponse.getProtocolVersion();
+        }
+
+        @Override
+        public boolean containsHeader(String name) {
+            return httpResponse.containsHeader(name);
+        }
+
+        @Override
+        public Header[] getHeaders(String name) {
+            return httpResponse.getHeaders(name);
+        }
+
+        @Override
+        public Header getFirstHeader(String name) {
+            return httpResponse.getFirstHeader(name);
+        }
+
+        @Override
+        public Header getLastHeader(String name) {
+            return httpResponse.getLastHeader(name);
+        }
+
+        @Override
+        public Header[] getAllHeaders() {
+            return httpResponse.getAllHeaders();
+        }
+
+        @Override
+        public void addHeader(Header header) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void addHeader(String name, String value) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void setHeader(Header header) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void setHeader(String name, String value) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void setHeaders(Header[] headers) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void removeHeader(Header header) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void removeHeaders(String name) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public HeaderIterator headerIterator() {
+            return httpResponse.headerIterator();
+        }
+
+        @Override
+        public HeaderIterator headerIterator(String name) {
+            return httpResponse.headerIterator(name);
+        }
+
+        @Override
+        @SuppressWarnings("deprecation")
+        public org.apache.http.params.HttpParams getParams() {
+            return httpResponse.getParams();
+        }
+
+        @Override
+        @SuppressWarnings("deprecation")
+        public void setParams(org.apache.http.params.HttpParams params) {
+            throw new UnsupportedOperationException();
         }
     }
 
